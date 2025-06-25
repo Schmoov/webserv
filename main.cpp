dfc523f0e324e756228332537745960ec0a28ab3
Paul Arden <parden@f4r10s13.clusters.42paris.fr>
2025-06-25 19:53:23 +0200

    first response

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <asm-generic/socket.h>
 #include <iostream>
 #include <sys/socket.h>
 //#include <sys/types.h>
@@ -8,12 +9,17 @@ using namespace std;
 
 int main() {
 	int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+	int opt = 1;
+	setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 	
-	sockaddr_in addr;
+	sockaddr_in addr {};
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = INADDR_ANY;
 	addr.sin_port = htons(8080);
-	bind(fd, (struct sockaddr *)&addr, sizeof(addr));
+
+	if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)))
+		cout << "FAIL\n";
 
 	listen(fd, 69);
 
@@ -21,7 +27,18 @@ int main() {
 	int sock = accept(fd, (struct sockaddr *)&addr, &sz);
 
 	char buf[100000];
+	while(1) {
 	read(sock, buf, sizeof(buf));
 
 	cout << buf << endl;
+
+	string resp =
+		"HTTP/1.1 200 OK\r\n"
+		"Content-Type: text/plain\r\n"
+		"Content-Length: 13\r\n"
+		"\r\n"
+		"Hello, world!";
+
+	write(sock, resp.c_str(), resp.size());
+	}
 }
