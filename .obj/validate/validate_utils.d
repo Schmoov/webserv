f68429ad15bcbb1fb99e636084cca394bc07a776
Kekevin <chantal@DESKTOP-MBHGT77>
2026-03-16 19:49:25 +0100

    debug wtf

--- .obj/validate/validate_utils.d
+++
@@ -1,9 +0,0 @@
-.obj/validate/validate_utils.o: src/validate/validate_utils.cpp \
- src/validate/../../inc/webserv.hpp src/validate/../../inc/enum.hpp \
- src/validate/../../inc/utils.hpp src/validate/../../inc/config.hpp \
- src/validate/../../inc/Conversation.hpp
-src/validate/../../inc/webserv.hpp:
-src/validate/../../inc/enum.hpp:
-src/validate/../../inc/utils.hpp:
-src/validate/../../inc/config.hpp:
-src/validate/../../inc/Conversation.hpp:
